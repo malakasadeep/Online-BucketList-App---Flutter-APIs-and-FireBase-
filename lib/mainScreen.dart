5dc6959086ebc7900809e0a82525b0a78a876688
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-07-26 00:46:56 +0530

    chore: Add internet and network state permissions, and integrate Dio package for API calls

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,11 +9,51 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<dynamic> bucketlist = [];
+  Future<void> getData() async {
+    try {
+      Response response = await Dio().get(
+          "https://flutter-fire-62db0-default-rtdb.firebaseio.com/list.json");
+      print(response);
+      setState(() {
+        bucketlist = response.data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("BucketList App"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: getData,
+            child: Text("Get Data"),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: bucketlist.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(bucketlist[index]["image"] ?? ""),
+                    ),
+                    title: Text(bucketlist[index]["item"] ?? ""),
+                    trailing: Text(bucketlist[index]["cost"].toString() ?? "0"),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
